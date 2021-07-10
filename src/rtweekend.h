30869f12f74854eeae56abefd70d8e5c324efa49
ADSWT518 <tangyazhou518@outlook.com>
2021-07-10 21:04:53 +0800

    finish chapter 8~9

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -27,13 +27,14 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-// Common Headers
-
-#include "ray.h"
-#include "vec3.h"
-
 inline double random_double() {
     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    static std::uniform_real_distribution<double> distribution(min, max);
     static std::mt19937 generator;
     return distribution(generator);
 }
@@ -47,5 +48,10 @@ inline double clamp(double x, double min, double max) {
     }
     return x;
 }
+
+// Common Headers
+
+#include "ray.h"
+#include "vec3.h"
 
 #endif //RAYTRACING_RTWEEKEND_H
--- src/vec3.h
+++ src/vec3.h
@@ -5,6 +5,8 @@
 #ifndef RAYTRACING_VEC3_H
 #define RAYTRACING_VEC3_H
 
+#include "rtweekend.h"
+
 #include <cmath>
 #include <iostream>
 
@@ -52,6 +54,20 @@ public:
 
     double length_squared() const {
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
+
+    inline static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    inline static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+    }
+
+    bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
     }
 
 public:
@@ -103,6 +119,33 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) {
+            continue;
+        }
+        return p;
+    }
+}
+
+vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+vec3 random_in_hemisphere(const vec3& normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0) {
+        return in_unit_sphere;          // In the same hemisphere as the normal
+    } else {
+        return -in_unit_sphere;         // Not in the same hemisphere
+    }
+}
+
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
 }
 
 #endif //RAYTRACING_VEC3_H
